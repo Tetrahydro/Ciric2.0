533c3e6666cb0fc00aa15e60b5347ed999dc29e2
Andrei Tudor <andrei.tudor93@gmail.com>
2018-07-29 16:29:03 +0300

    initial routing

--- app.js
+++ app.js
@@ -15,5 +15,35 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
+app.get('/cazare', function(req, res){
+    res.render('cazare');
+});
+
+app.get('/restaurant', function(req, res){
+    res.render('restaurant');
+});
+
+app.get('/conferinte', function(req, res){
+    res.render('conferinte');
+});
+
+app.get('/galerie', function(req, res){
+    res.render('galerie');
+});
+
+app.get('/evenimente', function(req, res){
+    res.render('evenimente');
+});
+
+app.get('/contact', function(req, res){
+    res.render('contact');
+});
+
+app.get('/agrement', function(req, res){
+    res.render('agrement');
+});
+
+
+
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
